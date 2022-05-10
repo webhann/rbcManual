ca97de8908ecb6e236cb8c5d7bcfb525804884ee
webhann <shuto@rbcv.co.jp>
2022-05-10 13:44:21 +0900

    modified style and metatag

---
+++ src/@lekoarts/gatsby-theme-cara/gatsby-plugin-theme-ui/components.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import ProjectCard from "../components/project-card"
+
+const components = {
+  // eslint-disable-next-line react/display-name
+  ProjectCard: ({ link, target, title, bg, children, ...props }) => (
+    <ProjectCard {...props} link={link} target={target} title={title} bg={bg}>
+      {children}
+    </ProjectCard>
+  ),
+}
+
+export default components
