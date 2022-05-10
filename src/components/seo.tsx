b6f04265fdf28ad1e94c48f3aa591003be7f574e
webhann <shuto@rbcv.co.jp>
2022-05-10 14:21:15 +0900

    added noindextag

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -48,11 +48,11 @@ const SEO = ({ title = ``, description = ``, pathname = ``, image = ``, children
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:image:alt" content={seo.description} />
       <meta name="twitter:creator" content={author} />
+      <meta name="robots" content="noindex, nofollow" />
       <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-cara" />
       <link rel="icon" type="image/png" sizes="32x32" href={withPrefix(`/favicon-32x32.png`)} />
       <link rel="icon" type="image/png" sizes="16x16" href={withPrefix(`/favicon-16x16.png`)} />
       <link rel="apple-touch-icon" sizes="180x180" href={withPrefix(`/apple-touch-icon.png`)} />
-      <meta name="robots" content="noindex, nofollow" />
       {children}
     </Helmet>
   )
